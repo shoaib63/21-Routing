494c0293390f460fab55e02bfa293e9ab3f4967b
shoaib63 <mirakishoaib@gmail.com>
2024-09-11 00:38:15 +0330

    start of 14

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HomePage from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from './pages/ProductDetail'
 
 
 const router = createBrowserRouter([
   { path: '/', element: <RootLayout /> , children: [
     {path: '/', element: <HomePage />} , 
     {path: '/products', element: <Products />} , 
+    {path: '/products/:productId' , element: <ProductDetailPage />}
   ] , 
   errorElement: <ErrorPage />
 },
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,4 +1,20 @@
+import { Link } from "react-router-dom"
+
+
+
+const PRODUCTS = [
+    {id: 'p1'  , title: 'Product 1'},
+    {id: 'p2'  , title: 'Product 2'},
+    {id: 'p3'  , title: 'Product 3'},
+]
 export default function Products() {
 
-    return <h1>The Products Page</h1>
+    return (
+        <>
+            <h1>The Products Page</h1>
+            <ul>
+                {PRODUCTS.map(item =><li key={item.id}><Link to={`/products/${item.id}`}>{item.title}</Link></li>)}
+            </ul>
+        </>
+    )
 }
